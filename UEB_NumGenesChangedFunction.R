701d20b24247e9821ba0a628d6265d9988bbaff9
rgonzalo <rgonzalo99@gmail.com>
2018-08-28 13:58:32 +0200

    se añade adjPvalue 0.15

--- UEB_NumGenesChangedFunction.R
+++ UEB_NumGenesChangedFunction.R
@@ -2,6 +2,7 @@
 ####################
 ##### geneSelectable
 ##### Autora: Miriam Mota
+##### Modificaciones: Ricardo Gonzalo
 ####################
 ##### topTab: Toptable en format '.csv'
 ##### adj0: p.valor adjustat pel qual filtrar (de normal, 0.01)
@@ -10,7 +11,7 @@
 ##### P1: p.valor pel qual filtrar (de normal, 0.01)
 ##### P2: segon p.valor pel qual filtrar (de normal, 0.05)
 ################################################################################
-genesSelectable <- function (topTab, adj0, adj1, adj2, P1, P2,FC=0)
+genesSelectable <- function (topTab, adj0, adj1, adj2, adj3, P1, P2,FC=0)
 {
   upBelowB <- sum(topTab$B > 0  & topTab$t > 0 & abs(topTab$logFC) > FC)
   downBelowB <- sum(topTab$B > 0  & topTab$t < 0 & abs(topTab$logFC) > FC)
@@ -24,6 +25,9 @@ genesSelectable <- function (topTab, adj0, adj1, adj2, P1, P2,FC=0)
   upBelowAdj2 <- sum(topTab$adj.P.Val < adj2 & topTab$t > 0 & abs(topTab$logFC) > FC)
   downBelowAdj2 <- sum(topTab$adj.P.Val < adj2 & topTab$t < 0 & abs(topTab$logFC) > FC)
   
+  upBelowAdj3 <- sum(topTab$adj.P.Val < adj3 & topTab$t > 0 & abs(topTab$logFC) > FC)
+  downBelowAdj3 <- sum(topTab$adj.P.Val < adj3 & topTab$t < 0 & abs(topTab$logFC) > FC)
+  
   upBelowP1 <- sum(topTab$P.Value < P1 & topTab$t > 0 & abs(topTab$logFC) > FC)
   downBelowP1 <- sum (topTab$P.Value < P1 & topTab$t < 0 & abs(topTab$logFC) > FC)
   
@@ -33,6 +37,7 @@ genesSelectable <- function (topTab, adj0, adj1, adj2, P1, P2,FC=0)
   return(c(upReg_B = upBelowB,downReg_B = downBelowB,
            upRegAdj0.01 = upBelowAdj0, downRegAdj0.01 = downBelowAdj0,
            upRegAdj0.05 = upBelowAdj1, downRegAdj0.05 = downBelowAdj1,
+           upRegAdj0.15 = upBelowAdj2, downRegAdj0.15 = downBelowAdj2,
            upRegAdj0.25 = upBelowAdj2, downRegAdj0.25 = downBelowAdj2,
            upRegP0.01 = upBelowP1, downRegP0.01 = downBelowP1,
            upRegP0.05 = upBelowP2, downRegP0.05 = downBelowP2))   
